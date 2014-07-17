0ba6fd8d17b77f9758c403f45e4e51098a849cf8
tooblue <oenkitt@gmail.com>
2014-07-17 17:39:16 -0500

    Added robots meta tag

--- etcg/login.php
+++ etcg/login.php
@@ -31,6 +31,7 @@ if ( $enablelogin ) {
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="noindex, nofollow, noarchive">
 
 		<title>EasyTCG FM | Login</title>
 
