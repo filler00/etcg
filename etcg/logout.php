95f2f717eca93c1397748bbd4bb87e71543ee794
tooblue <oenkitt@gmail.com>
2014-07-17 17:39:56 -0500

    Added robots meta tag

--- etcg/logout.php
+++ etcg/logout.php
@@ -13,6 +13,7 @@ $session->close();
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="robots" content="noindex, nofollow, noarchive">
 
 		<title>EasyTCG FM | Login</title>
 
